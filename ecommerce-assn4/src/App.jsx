043b3ba72cce614c041ed178d648d21c39003d07
Jeff Jia <mj547@students.waikato.ac.nz>
2026-06-02 12:56:46 +1200

    create cartcontext

--- ecommerce-assn4/src/App.jsx
+++ ecommerce-assn4/src/App.jsx
@@ -3,16 +3,27 @@ import { CartPage } from './pages/CartPage'
 import { ProductsPage } from './pages/ProductsPage'
 import { NavBar } from './components/Navbar'
 import { ProductDetailsPage } from './pages/ProductDetailsPage'
+import { useState } from 'react'
+import { CartContext } from './context/CartContext'
 
 function App() {
+    const [cart, setCart] = useState([])
+
+    function addToCart() {
+        if (cart) {
+        }
+    }
+
     return (
         <>
             <NavBar />
-            <Routes>
-                <Route path="/" element={<ProductsPage />}></Route>
-                <Route path="/cart" element={<CartPage />}></Route>
-                <Route path="/details/:id" element={<ProductDetailsPage />}></Route>
-            </Routes>
+            <CartContext.Provider value={{ cart, addToCart }}>
+                <Routes>
+                    <Route path="/" element={<ProductsPage />}></Route>
+                    <Route path="/cart" element={<CartPage />}></Route>
+                    <Route path="/details/:id" element={<ProductDetailsPage />}></Route>
+                </Routes>
+            </CartContext.Provider>
         </>
     )
 }
